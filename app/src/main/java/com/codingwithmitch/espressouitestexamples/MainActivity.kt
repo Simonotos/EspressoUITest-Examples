da90bcb64c9f238054979697dba674e1d0a8a573
Simone Crocco <simonotosdev@gmail.com>
2023-09-29 12:30:29 +0200

    initialize project #2
    removed test to write

--- app/src/main/java/com/codingwithmitch/espressouitestexamples/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/espressouitestexamples/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity(){
 
     private val TAG: String = "AppDebug"
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
